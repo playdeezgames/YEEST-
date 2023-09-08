516a7e29951164a660b146bb45ba53417afef184
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-08 04:07:51 -0500

    serialize data to json

--- src/YEEST.Persistence.Tests/WorldShould.vb
+++ src/YEEST.Persistence.Tests/WorldShould.vb
@@ -1,21 +1,23 @@
 Imports Shouldly
 Imports Xunit
+Imports YEEST.Data
+
 Public Class WorldShould
     <Fact>
     Sub be_instantiable()
-        Dim subject As IWorld = World.Create()
+        Dim subject As IWorld = World.Create(New WorldData)
 
         subject.ShouldNotBeNull
     End Sub
     <Fact>
     Sub serialize()
-        Dim subject As IWorld = World.Create()
+        Dim subject As IWorld = World.Create(New WorldData)
 
-        subject.SerializedData.ShouldBe("{}")
+        subject.SerializedData.ShouldBe("{""Locations"":[]}")
     End Sub
     <Fact>
     Sub create_location()
-        Dim subject As IWorld = World.Create()
+        Dim subject As IWorld = World.Create(New WorldData)
 
         Dim actual = subject.CreateLocation()
 
--- src/YEEST.Persistence/World.vb
+++ src/YEEST.Persistence/World.vb
@@ -1,17 +1,21 @@
+Imports System.Text.Json
+Imports YEEST.Data
+
 Public Class World
     Implements IWorld
-    Private Sub New()
-
+    Private ReadOnly data As WorldData
+    Private Sub New(data As WorldData)
+        Me.data = data
     End Sub
 
     Public ReadOnly Property SerializedData As String Implements IWorld.SerializedData
         Get
-            Return "{}"
+            Return JsonSerializer.Serialize(data)
         End Get
     End Property
 
-    Public Shared Function Create() As IWorld
-        Return New World()
+    Public Shared Function Create(data As WorldData) As IWorld
+        Return New World(data)
     End Function
 
     Public Function CreateLocation() As ILocation Implements IWorld.CreateLocation
