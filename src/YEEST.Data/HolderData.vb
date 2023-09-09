6aa42ac6b558795f77acd3ccf50e4395688cde62
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-09 05:14:52 -0500

    added flags to things

--- src/YEEST.Data/HolderData.vb
+++ src/YEEST.Data/HolderData.vb
@@ -1,3 +1,4 @@
 ﻿Public MustInherit Class HolderData
     Public Property Metadatas As New Dictionary(Of String, String)
+    Public Property Flags As New HashSet(Of String)
 End Class
--- src/YEEST.Persistence.Tests/WorldShould.vb
+++ src/YEEST.Persistence.Tests/WorldShould.vb
@@ -18,7 +18,7 @@ Public Class WorldShould
 
         Dim actual = subject.SerializedData
 
-        actual.ShouldBe("{""Locations"":[],""Characters"":[],""Metadatas"":{}}")
+        actual.ShouldBe("{""Locations"":[],""Characters"":[],""Metadatas"":{},""Flags"":[]}")
     End Sub
     <Fact>
     Sub create_location_when_calling_CreateLocation()
@@ -29,7 +29,7 @@ Public Class WorldShould
 
         actual.ShouldNotBeNull()
         data.Locations.ShouldHaveSingleItem()
-        subject.SerializedData.ShouldBe("{""Locations"":[{""Metadatas"":{}}],""Characters"":[],""Metadatas"":{}}")
+        subject.SerializedData.ShouldBe("{""Locations"":[{""Metadatas"":{},""Flags"":[]}],""Characters"":[],""Metadatas"":{},""Flags"":[]}")
     End Sub
     <Fact>
     Sub create_character_when_calling_CreateCharacter()
@@ -40,7 +40,7 @@ Public Class WorldShould
 
         actual.ShouldNotBeNull()
         data.Characters.ShouldHaveSingleItem()
-        subject.SerializedData.ShouldBe("{""Locations"":[],""Characters"":[{""Metadatas"":{}}],""Metadatas"":{}}")
+        subject.SerializedData.ShouldBe("{""Locations"":[],""Characters"":[{""Metadatas"":{},""Flags"":[]}],""Metadatas"":{},""Flags"":[]}")
     End Sub
     Protected Overrides Sub ValidateSetMetadata(key As String, value As String, data As WorldData, subject As IWorld)
         subject.SerializedData.ShouldContain(key)
