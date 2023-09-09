145efc01b9e4fd4627eca5ff6d48c6581ee2245c
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-09 04:58:08 -0500

    holdershould consolidates metadata tests

--- src/YEEST.Persistence.Tests/HolderShould.vb
+++ src/YEEST.Persistence.Tests/HolderShould.vb
@@ -1,4 +1,6 @@
-﻿Imports Xunit
+﻿Imports Shouldly
+Imports Xunit
+Imports YEEST.Data
 
 Public MustInherit Class HolderShould(Of THolder As IHolder)
     Protected MustOverride Function CreateSubject(data As Data.WorldData) As THolder
@@ -6,28 +8,68 @@ Public MustInherit Class HolderShould(Of THolder As IHolder)
     Protected MustOverride Sub ValidateRemoveMetadata(key As String, value As String, data As Data.WorldData, subject As THolder)
     <Fact>
     Public Sub retrieve_metadata_when_calling_GetMetadata()
-        DoGetMetadataTest(AddressOf CreateSubject)
+        Const MetadataKey = "key"
+        Const MetadataValue = "value"
+        Dim data = New WorldData
+        Dim subject As IHolder = CreateSubject(data)
+        subject.SetMetadata(MetadataKey, MetadataValue)
+
+        Dim actual As String = subject.GetMetadata(MetadataKey)
+
+        actual.ShouldBe(MetadataValue)
     End Sub
     <Fact>
     Public Sub throw_exception_when_metadata_key_not_found_when_calling_GetMetadata()
-        DoGetMetadataNotFoundTest(AddressOf CreateSubject)
+        Const MetadataKey = "key"
+        Dim data = New WorldData
+        Dim subject = CreateSubject(data)
+
+        Dim action = Sub() subject.GetMetadata(MetadataKey)
+
+        Should.Throw(Of KeyNotFoundException)(action)
     End Sub
     <Fact>
     Public Sub return_false_when_metadata_key_not_found_when_calling_HasMetadata()
-        DoHasMetadataNotFoundTest(AddressOf CreateSubject)
+        Const MetadataKey = "key"
+        Dim data = New WorldData
+        Dim subject = CreateSubject(data)
+
+        Dim actual = subject.HasMetadata(MetadataKey)
+
+        actual.ShouldBeFalse
     End Sub
     <Fact>
     Public Sub return_true_when_metadata_key_id_found_when_calling_HasMetadata()
-        DoHasMetadataTest(AddressOf CreateSubject)
+        Const MetadataKey = "key"
+        Const MetadataValue = "value"
+        Dim data = New WorldData
+        Dim subject = CreateSubject(data)
+        subject.SetMetadata(MetadataKey, MetadataValue)
+
+        Dim actual = subject.HasMetadata(MetadataKey)
+
+        actual.ShouldBeTrue
     End Sub
     <Fact>
     Public Sub store_metadata_when_calling_SetMetadata()
-        DoSetMetadataTest(AddressOf CreateSubject,
-                          AddressOf ValidateSetMetadata)
+        Const MetadataKey = "key"
+        Const MetadataValue = "value"
+        Dim data = New Data.WorldData
+        Dim subject = CreateSubject(data)
+        subject.SetMetadata(MetadataKey, MetadataValue)
+        ValidateSetMetadata(MetadataKey, MetadataValue, data, subject)
     End Sub
     <Fact>
     Public Sub remove_metadata_after_adding_it_when_calling_RemoveMetadata()
-        DoRemoveMetadataTest(AddressOf CreateSubject,
-                             AddressOf ValidateRemoveMetadata)
+        Const MetadataKey = "key"
+        Const MetadataValue = "value"
+        Dim data = New WorldData
+        Dim subject = CreateSubject(data)
+        subject.SetMetadata(MetadataKey, MetadataValue)
+
+        subject.RemoveMetadata(MetadataKey)
+
+        subject.HasMetadata(MetadataKey).ShouldBeFalse
+        ValidateRemoveMetadata(MetadataKey, MetadataValue, data, subject)
     End Sub
 End Class
