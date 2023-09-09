496ee84887b9a345fb1a8e5a3ae0a58f90d71f51
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-09 04:53:15 -0500

    location should comes fro mholdershould

--- src/YEEST.Persistence.Tests/LocationShould.vb
+++ src/YEEST.Persistence.Tests/LocationShould.vb
@@ -3,7 +3,7 @@ Imports Xunit
 Imports YEEST.Data
 
 Public Class LocationShould
-
+    Inherits HolderShould(Of ILocation)
     <Fact>
     Sub have_a_location_id()
         Dim w = World.Create(New Data.WorldData)
@@ -13,34 +13,16 @@ Public Class LocationShould
         subject.ShouldNotBeNull
         subject.Id.ShouldBe(0)
     End Sub
-    Private Shared Function CreateSubject(data As Data.WorldData) As ILocation
+
+    Protected Overrides Sub ValidateSetMetadata(key As String, value As String, data As WorldData, subject As ILocation)
+        'do nothing
+    End Sub
+
+    Protected Overrides Sub ValidateRemoveMetadata(key As String, value As String, data As WorldData, subject As ILocation)
+        'do nothing
+    End Sub
+
+    Protected Overrides Function CreateSubject(data As WorldData) As ILocation
         Return World.Create(data).CreateLocation
     End Function
-    <Fact>
-    Sub store_metadata_when_calling_SetMetadata()
-        DoSetMetadataTest(AddressOf CreateSubject,
-            Sub(k, v, d, s)
-                d.Locations(0).Metadatas.ShouldContainKeyAndValue(k, v)
-            End Sub)
-    End Sub
-    <Fact>
-    Sub retrieve_metadata_when_calling_GetMetadata()
-        DoGetMetadataTest(AddressOf CreateSubject)
-    End Sub
-    <Fact>
-    Sub throw_exception_when_metadata_key_not_found_when_calling_GetMetadata()
-        DoGetMetadataNotFoundTest(AddressOf CreateSubject)
-    End Sub
-    <Fact>
-    Sub return_false_when_metadata_key_not_found_when_calling_HasMetadata()
-        DoHasMetadataNotFoundTest(AddressOf CreateSubject)
-    End Sub
-    <Fact>
-    Sub return_true_when_metadata_key_id_found_when_calling_HasMetadata()
-        DoHasMetadataTest(AddressOf CreateSubject)
-    End Sub
-    <Fact>
-    Sub remove_metadata_after_adding_it_when_calling_RemoveMetadata()
-        DoRemoveMetadataTest(AddressOf CreateSubject)
-    End Sub
 End Class
