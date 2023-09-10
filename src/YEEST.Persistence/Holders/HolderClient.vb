34db4a0dfec18c849c3b9b09c006f324150ce957
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-10 04:00:11 -0500

    HasStatistic refactor

--- src/YEEST.Persistence/Holders/HolderClient.vb
+++ src/YEEST.Persistence/Holders/HolderClient.vb
@@ -37,7 +37,7 @@
     End Function
 
     Public Function GetStatistic(statisticName As String, Optional defaultValue As Integer = 0) As Integer Implements IStatisticHolder.GetStatistic
-        Return If(StatisticSource.ContainsKey(statisticName), StatisticSource(statisticName), defaultValue)
+        Return If(HasStatistic(statisticName), StatisticSource(statisticName), defaultValue)
     End Function
 
     Public Function HasStatistic(statisticName As String) As Boolean Implements IStatisticHolder.HasStatistic
