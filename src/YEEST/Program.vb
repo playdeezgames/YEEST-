770e4f9a5b00ad58e6cfa6cf0b10a2791abfba93
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-11 05:37:23 -0500

    folders!

--- src/YEEST/Program.vb
+++ src/YEEST/Program.vb
@@ -1,5 +1,6 @@
 Module Program
     Sub Main(args As String())
+        Console.Title = "YEEST!!"
         Dim host As IHost = New Host(AddressOf AnsiConsole.MarkupLine, AddressOf AnsiConsole.Ask)
         host.Run()
     End Sub
