952a63ce730efc480b5fb43526ded5cd8d557a23
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-11 04:59:41 -0500

    i've got a passing test

--- src/YEEST.Presentation.Tests/HostShould.vb
+++ src/YEEST.Presentation.Tests/HostShould.vb
@@ -7,13 +7,14 @@ Namespace YEEST.Presentation.Tests
         Sub run()
             Dim output As New List(Of String)
             Dim input As New Queue(Of String)
-            input.Enqueue(String.Empty)
+            input.Enqueue("quit")
+            input.Enqueue("yes")
             Dim subject As IHost = New Host(AddressOf output.Add, AddressOf input.Dequeue)
 
             Dim action = Sub() subject.Run()
 
             Should.NotThrow(action)
-            output.Count.ShouldBe(1)
+            output.Count.ShouldBe(2)
             input.ShouldBeEmpty
         End Sub
     End Class
