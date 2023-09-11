d37eef99d4e2a86f40b46d0bb83c345356452369
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-11 05:50:19 -0500

    quitting tests

--- src/YEEST.Presentation.Tests/HostShould.vb
+++ src/YEEST.Presentation.Tests/HostShould.vb
@@ -3,18 +3,21 @@ Imports Shouldly
 
 Namespace YEEST.Presentation.Tests
     Public Class HostShould
-        <Fact>
-        Sub run()
+        <Theory>
+        <InlineData(2, "quit", "yes")>
+        <InlineData(4, "quit", "no", "quit", "yes")>
+        Sub run(outputCount As Integer, ParamArray commands() As String)
             Dim output As New List(Of String)
             Dim input As New Queue(Of String)
-            input.Enqueue("quit")
-            input.Enqueue("yes")
+            For Each cmd In commands
+                input.Enqueue(cmd)
+            Next
             Dim subject As IHost = New Host(AddressOf output.Add, Function(p, d) input.Dequeue)
 
             Dim action = Sub() subject.Run()
 
             Should.NotThrow(action)
-            output.Count.ShouldBe(2)
+            output.Count.ShouldBe(outputCount)
             input.ShouldBeEmpty
         End Sub
     End Class
