b61d85c5de7868987c249490232cbfce05195139
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-12 04:31:18 -0500

    rearranged constants

--- src/YEEST.Presentation/Constants.vb
+++ src/YEEST.Presentation/Constants.vb
@@ -1,12 +1,15 @@
 ﻿Friend Module Constants
+    Friend Const InvalidCommandMessage = "[red]Invalid Command![/]"
+
+    Friend Const DefaultPrompt = "[white]>[/]"
+
+    Friend Const ConfirmQuitState As String = "ConfirmQuit"
+    Friend Const HelpState = "Help"
+    Friend Const TitleState As String = "Title"
+
+    Friend Const ExitText = "exit"
+    Friend Const HelpText = "help"
+    Friend Const NoText = "no"
     Friend Const QuitText = "quit"
     Friend Const YesText = "yes"
-    Friend Const NoText = "no"
-    Friend Const HelpText = "help"
-    Friend Const TitleState As String = "title"
-    Friend Const ConfirmQuitState As String = "ConfirmQuit"
-    Friend Const HelpState = "help"
-    Friend Const DefaultPrompt = "[white]>[/]"
-    Friend Const ExitText = "exit"
-    Friend Const InvalidCommandMessage = "[red]Invalid Command![/]"
 End Module
